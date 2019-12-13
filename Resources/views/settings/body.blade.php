6cd68e36f745eed1fc969a98e6c3184ab16c9424
Your Name <you@example.com>
2019-12-13 13:42:07 -0300

    .

--- Resources/views/settings/body.blade.php
+++ Resources/views/settings/body.blade.php
@@ -1,3 +1,11 @@
+@if($event_setting->alert)
+<div class="alert alert-info alert-dismissible fade show" role="alert">
+	 O número inicial do pedido será a sequência do número de novos pedidos. Caso possui uma integração com o sistema da Scancode, aconselhamos a iniciar com o último número do pedido do seu seu ERP + 100.
+	{{ Form::open(['route' => ['portal.event_settings.update', $event_setting], 'method' => 'put']) }}
+	{{ Form::button('<span aria-hidden="true">&times;</span>', ['type' => 'submit', 'class' => 'close']) }}
+	{{ Form::close() }}
+</div>
+@endif
 
 {{ Form::model($event_setting->configurable, ['route' => ['settingorder.update', $event_setting->configurable], 'method' => 'put']) }}
 <div class="form-group">
